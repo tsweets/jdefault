16da939336b3a6d5ef17b012368c1b874f8e2a18
Tony Sweets <tsweets@readyfunds.net>
2014-02-18 21:30:48 -0700

    updated docs

--- src/main/java/org/beer30/jdefault/DefaultBase.java
+++ src/main/java/org/beer30/jdefault/DefaultBase.java
@@ -21,7 +21,6 @@ import org.apache.commons.lang.math.RandomUtils;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
-import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -43,13 +42,17 @@ public class DefaultBase {
         Yaml yaml = new Yaml();
         String languageCode = "en";
 
-
-        URL dictionaryUrl = Address.class.getClassLoader().getResource("META-INF/en.yml");
         Map valuesMap = (Map) yaml.load(findStream("META-INF/en.yml"));
         valuesMap = (Map) valuesMap.get(languageCode);
         fakeValuesMap = (Map<String, Object>) valuesMap.get("faker");
     }
 
+    /**
+     * Finds the InputStream of a file in a Jar
+     *
+     * @param filename of the stream to return
+     * @return the file's InputStream
+     */
     private static InputStream findStream(String filename) {
         InputStream streamOnClass = Address.class.getResourceAsStream(filename);
         if (streamOnClass != null) {
@@ -60,10 +63,10 @@ public class DefaultBase {
     }
 
     /**
-     * Fetches an Object from the dictionary
+     * Fetches a random Object from the dictionary based on a key
      *
-     * @param key
-     * @return
+     * @param key of the object to find
+     * @return the Object that represents the key
      */
     protected static Object fetch(String key) {
         List valuesArray = (List) fetchObject(key);
@@ -73,13 +76,13 @@ public class DefaultBase {
     /**
      * Fetches a List of of Values
      *
-     * @param key
-     * @return
+     * @param key of the values to find
+     * @return random value from each list
      */
     protected static String fetchList(String key) {
         List valuesArray = (List) fetchObject(key);
 
-        //TODO: Make this dynamic
+        //TODO: Make this dynamic - assumes 3 lists
         List<String> stringList1 = (List<String>) valuesArray.get(0);
         List<String> stringList2 = (List<String>) valuesArray.get(1);
         List<String> stringList3 = (List<String>) valuesArray.get(2);
@@ -88,6 +91,12 @@ public class DefaultBase {
 
     }
 
+    /**
+     * Fetches a value
+     *
+     * @param key of the value to find
+     * @return the value converted to type String
+     */
     protected static String fetchString(String key) {
         List<String> stringList = (List<String>) fetchObject(key);
         return stringList.get(RandomUtils.nextInt(stringList.size()));
@@ -95,6 +104,11 @@ public class DefaultBase {
 
     }
 
+    /**
+     * Finds map of values based on a key
+     * @param key to lookup
+     * @return map of key value pairs
+     */
     protected static Object fetchObject(String key) {
         String[] path = key.split("\\.");
         Object currentValue = fakeValuesMap;
@@ -104,6 +118,11 @@ public class DefaultBase {
         return currentValue;
     }
 
+    /**
+     * Replace '#' in a string with random numbers
+     * @param numberString to insert random numbers
+     * @return a string with random numbers
+     */
     protected static String numerify(String numberString) {
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < numberString.length(); i++) {
@@ -117,6 +136,11 @@ public class DefaultBase {
         return sb.toString();
     }
 
+    /**
+     * Replace '?' in a string with random letters
+     * @param letterString to insert random letters
+     * @return a string with random letters
+     */
     protected static String letterify(String letterString) {
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < letterString.length(); i++) {
@@ -130,15 +154,32 @@ public class DefaultBase {
         return sb.toString();
     }
 
+    /**
+     * Run both numerify and letterfy on a string
+     * @param string to run the replacement on
+     * @return replaced string
+     */
     protected static String bothify(String string) {
         return letterify(numerify(string));
     }
 
+    /**
+     * Resolve embedded key
+     * for example  - "#{name.first_name} #{name.last_name}" is a value for the the key name.name
+     * embedded in this value are 2 other keys that need to be resolved
+     * @param key that links to an embedded key
+     * @return fully resolved string
+     */
     protected static String parse(String key) {
         String value = (String) fetch(key);
         return parseFoundKey(value);
     }
 
+    /**
+     * resolves an embedded key
+     * @param value embedded key that has been already fetched
+     * @return fully resolved string
+     */
     protected static String parseFoundKey(String value) {
         //   System.out.println("Start Value: " + value);
 
